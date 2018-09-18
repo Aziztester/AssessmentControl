304bed7d8cea5993c0631cd95163b631b00cd8e7
enkhbat <e_munkhbold@yahoo.com>
2018-09-18 15:58:38 -0700

    [ADDED]Test automation for TCs ASK-243, ASK-244, ASK-245

--- src/main/java/study/qa/automation/stepDefinitions/QuizStepDef.java
+++ src/main/java/study/qa/automation/stepDefinitions/QuizStepDef.java
@@ -10,6 +10,7 @@ import static org.assertj.core.api.Assertions.*;
 import static study.qa.automation.utils.TestContext.getDriver;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.interactions.Actions;
 
 public class QuizStepDef {
     @Then("^I scroll to the element with xpath \"([^\"]*)\" with offset (\\d+)$")
@@ -20,5 +21,26 @@ public class QuizStepDef {
         executor.executeScript("window.scrollBy(0, " + offset + ");", element);
         Thread.sleep(500);
     }
+
+
+    @Then("^I move the slider to rate ([^\"]*)$")
+    public void iMoveTheSliderToRate(String Rate) throws Throwable {
+        //WebElement slider = getDriver().findElement(By.xpath("//mat-slider[@role='slider'][@aria-valuenow=5]"));
+        WebElement slider = getDriver().findElement(By.xpath("//div[@class='controls']//div[@class='mat-slider-thumb']\n"));
+        Actions move = new Actions(getDriver());
+        int num = (int)Math.round((75 - Double.parseDouble(Rate)) * 1.13);
+        move.dragAndDropBy(slider, -num, 0).build().perform();
+        Thread.sleep(1000);
+    }
+
+   /* @Then("^I move the slider to rate \"([^\"]*)\"$")
+    public void iMoveTheSliderToRate(String rate) {
+        // Write code here that turns the phrase above into concrete actions
+        WebElement slider = getDriver().findElement(By.xpath("//div[@class='controls']//div[@class='mat-slider-thumb']\n"));
+        Actions move = new Actions(getDriver());
+        int num = (int)Math.round((75 - Double.parseDouble(rate)) * 1.1);
+        move.dragAndDropBy(slider, -num, 0).build().perform();
+        Thread.sleep(1000);
+    }*/
 }
 
